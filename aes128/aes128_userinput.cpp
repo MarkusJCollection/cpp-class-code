1d528e4adddd97d74a8c55a6a99f96144943482f
MarkusJCollection <markusjosephj@gmail.com>
2026-09-23 15:06:24 -0500

    Small edits.

--- aes128/aes128_userinput.cpp
+++ aes128/aes128_userinput.cpp
@@ -17,7 +17,7 @@ int askOption() {
 	cout << "\n\nPlease choose an option by typing the appropriate number -" << "\n";
 	cout << "\t1: Change directory path.\n";
 	cout << "\t2: Change AES Key.\n";
-	cout << "\t3: Create an initial test file to encrypt and decrypt.\n";
+	cout << "\t3: Encrypt and decrypt the showcase file.\n";
 	cout << "\t4: Encrypt your own file.\n";
 	cout << "\t5: Decrypt your own file.\n";
 	cout << "\t6: Exit the program.\n";
